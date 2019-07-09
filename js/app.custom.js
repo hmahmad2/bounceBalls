356d6848be2d1234b94d72c29f0c4bcb89555b3f
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-07-09 12:58:09 -0500

    setting up my canvas

--- js/app.custom.js
+++ js/app.custom.js
@@ -1,1 +1,10 @@
 // alert(123);
+var canvas = document.getElementById("ballCanvas");
+// get methods for a 2d canvas
+var can = canvas.getContext("2d");
+// width and height of the browser window
+var canWidth = window.innerWidth;
+var canHeight = window.innerHeight;
+// set the canvas to be the width and height of the window
+canvas.width = canWidth;
+canvas.height = canHeight;
