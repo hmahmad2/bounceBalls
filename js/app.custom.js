1eeaa6679b74cafabd78eb77ac87d6cbc6dcfc80
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-07-29 11:52:13 -0500

    added color randomizer to the balls

--- js/app.custom.js
+++ js/app.custom.js
@@ -9,16 +9,31 @@ var canHeight = window.innerHeight;
 canvas.width = canWidth;
 canvas.height = canHeight;
 
+var mHeight = 0;
+var mWidth = 0;
+
 // an event handler for knowing when the mouse location
 addEventListener("mousepos", function() {
    mHeight = event.clientX;
    mWidth = event.clientY;
 });
 
+function randomColor() {
+   return (
+      "rgb(" +
+      Math.round(Math.random() * 250) + "," +
+      Math.round(Math.random() * 250) + "," +
+      Math.round(Math.random() * 250)
+      + ")"
+   );
+}
+
 // since this is some sort of dynamic object, I think I'll make it a function
 function BounceBall() {
    this.radius = Math.random() * 10 + 10;
    this.x = Math.random() * (canWidth - this.radius * 2) + this.radius;
    this.y = Math.random() * (canHeight - this.radius * 2) + this.radius;
-
+   this.color = randomColor();
+   this.dy = Math.random() * 2;
+   this.dx = Math.round((Math.random() - 0.5) * 10);
 }
